c4c03941f70b678c20365733d414b64e93eaadcf
Bartek <bartekrakoczy1@gmail.com>
2026-04-15 16:52:54 +0200

    Replace tracked backups with ignored local backup folder

--- Backups/card_editor_pre_power_timing_fix_20260406-034151/mods/card_editor/CardEditorOverrideKeys.cs
+++
@@ -1,7 +0,0 @@
-namespace SlayTheSpire2Mod.CardEditor;
-
-internal static class CardEditorOverrideKeys
-{
-	public const string ResonanceEnemyStrengthLoss = "CardEditor.Resonance.EnemyStrengthLoss";
-}
-
